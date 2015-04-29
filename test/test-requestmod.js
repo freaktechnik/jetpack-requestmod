66a4202ca814df72c40898afcf35100733b89730
Martin Giger <martin.a.giger@sunrise.ch>
2015-04-29 19:13:45 +0200

    More unit tests

--- test/test-requestmod.js
+++ test/test-requestmod.js
@@ -9,6 +9,8 @@
 const { Request } = require("sdk/request");
 const { RequestMod } = require("../lib/requestmod");
 const { startServerAsync } = require("./httpd");
+const { CC } = require("chrome");
+const ScriptableInputStream = CC("@mozilla.org/scriptableinputstream;1", "nsIScriptableInputStream", "init");
 
 const CONTENT = "<h1>Test</h1>";
 let srv = startServerAsync(-1);
@@ -16,6 +18,23 @@ srv.registerPathHandler("/", function(request, response) {
     response.processAsync();
     response.setHeader('Content-Type', 'text/html', false);
     response.write(CONTENT);
+    response.finish();
+});
+
+srv.registerPathHandler("/echo/", function(request, response) {
+    response.processAsync();
+    response.setHeader('Content-Type', 'application/json', false);
+    let obj = {};
+    obj.content = new ScriptableInputStream(request.bodyInputStream).read(4);
+    let headers = {}, h = request.headers, s;
+    while(h.hasMoreElements()) {
+        s = h.getNext().toString();
+        headers[s] = request.getHeader(s);
+    }
+    obj.headers = headers;
+    obj.method = request.method;
+
+    response.write(JSON.stringify(obj));
     response.finish();
 });
 
@@ -30,8 +49,11 @@ function compareHeaders(headers, expected) {
 
 exports['test outgoing'] = function(assert, done) {
     var r = Request({
-        url: ROOT,
-        onComplete: () => {
+        url: ROOT + "echo/",
+        content: "test",
+        onComplete: (res) => {
+            assert.equal(res.json.content, "tset");
+            assert.equal(res.json.method, "PUT");
             mod.destroy();
             done();
         }
@@ -41,15 +63,18 @@ exports['test outgoing'] = function(assert, done) {
         direction: [ RequestMod.OUTGOING ],
         requestHandler: function(req) {
             assert.equal(req.url, r.url, "URL property works correctly");
-            assert.equal(req.method, 'GET', "Method matches");
+            assert.equal(req.method, 'POST', "Method matches");
             assert.equal(req.direction, RequestMod.OUTGOING, "Direction is correct");
             assert.equal(req.referrer, null, "Referrer is matching");
             assert.throws(() => req.status, "Status is unavailable for outgoing requests");
+            assert.equal(req.content, "test", "Content is read correctly");
 
-            //TODO test modifying stuff -> needs httpd to return the changed stuff
+            req.content = "tset";
+            req.referrer = "http://humanoids.be";
+            req.method = 'PUT';
         }
     });
-    r.get();
+    r.post();
 };
 
 exports['test incoming'] = function(assert, done) {
@@ -123,5 +148,28 @@ exports['test abort incoming'] = function(assert, done) {
     });
     r.get();
 };
+
+exports['test redirect'] = function(assert, done) {
+    var r = Request({
+        url: ROOT + "echo/",
+        onComplete: function(res) {
+            assert.equal(res.text, CONTENT, "Request successfully redirected");
+            mod.destroy();
+            done();
+        }
+    });
+    var mod = RequestMod({
+        url: 'http://localhost*',
+        direction: [ RequestMod.OUTGOING ],
+        requestHandler: function(req) {
+            // We will get the request again after the redirect!
+            if(req.url != ROOT)
+                req.url = ROOT;
+        }
+    });
+    r.get();
+};
+
+//TODO test contracts
 
 require("sdk/test").run(exports);
